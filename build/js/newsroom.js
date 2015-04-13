ca4066c9de8de9b2f9b48a2a6d2bd2ff7e767cc7
Carlos Ortega <carlos@goodrx.com>
2015-04-13 05:46:42 -0700

    add newsroom, take a nap

---
+++ build/js/newsroom.js
@@ -0,0 +1,31 @@
+(function(){
+    $('#news-body').dcSocialStream({
+        feeds: {
+            twitter: {
+                id: '#jclawcpa,JC_LawCPA',
+                retweets: true,
+                thumb: true
+            },
+            facebook: {
+                id: '1394483740809502'
+            },
+            youtube: {
+                id: 'irsvideos, UC2A6gNuULMFPd107sGowphg'
+            }
+
+        },
+        rotate: {
+            delay: 0
+        },
+        twitterId: 'JC_LawCPA',
+        control: false,
+        filter: false,
+        wall: true,
+        cache: false,
+        max: 'limit',
+        limit: 5,
+        transition: '0.3s',
+        iconPath: 'img/dcsns-dark/',
+        imagePath: 'img/dcsns-dark/'
+    });
+}());
